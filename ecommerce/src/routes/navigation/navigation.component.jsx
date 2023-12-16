4d65e9bddba0ec4b9c01416e6b14ccf561afd40c
D-coder20 <debpriya.20@gmail.com>
2023-12-16 12:32:08 +0530

    added our sign in component as well as firebase utils nad brought in firebase libraries

---
+++ ecommerce/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrawnLogo } from "../../assests/crown.svg";
+import "./navigation.styles.scss";
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrawnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container"></div>
+        <Link className="nav-link" to="/shop">
+          Shop
+        </Link>
+        <Link className="nav-link" to="/signin">
+          Sign-in
+        </Link>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
---
+++ ecommerce/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,70 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore"; //doc-retrieve document from firebase database
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgDfrmdGDh6IEPoseqazKEk1kRXRaGVJQ",
+  authDomain: "ecommerce-db-c0d8d.firebaseapp.com",
+  projectId: "ecommerce-db-c0d8d",
+  storageBucket: "ecommerce-db-c0d8d.appspot.com",
+  messagingSenderId: "124089658997",
+  appId: "1:124089658997:web:e357b8c87935c95ce1d23c",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider(); //which internally give you back the provider. googleauthprovider is a class
+
+provider.setCustomParameters({
+  prompt: "select_account",
+}); //setcustomparameters take some kind of configuration object and with it we can tell
+// different ways that we want this googleauthprovider to behave
+//everytime somebody interacts with our provider we wanna always force them to select the account
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//create the db
+export const db = getFirestore();
+
+//we want some fn that will take that data getting from the authentication service and the we gonna store that inside of the firestor
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user ", error.message);
+    }
+  }
+  return userDocRef;
+  //if user data does not exist
+  //if user data exist
+  //return userDocRef
+};
