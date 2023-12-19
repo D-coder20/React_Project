2bb80ef6a588f1a2016ea3bb373d2e2b523f1f1a
D-coder20 <debpriya.20@gmail.com>
2023-12-19 20:13:17 +0530

    created authentication page component alongside signin form

--- ecommerce/src/routes/authentication/authentication.component.jsx
+++ ecommerce/src/routes/authentication/authentication.component.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+
 // import { useEffect } from "react";
 // import { getRedirectResult } from "firebase/auth";
-import {
-  auth,
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
-} from "../../utils/firebase/firebase.utils";
+// import {
+//   auth,
+//   signInWithGooglePopup,
+//   createUserDocumentFromAuth,
+//   signInWithGoogleRedirect,
+// } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form copy/sign-in-form.component";
+import "./authentication.styles.scss";
 
-const SignIn = () => {
+const Authentication = () => {
   //   const userRedirect = async () => {
   //     const response = await getRedirectResult(auth);
   //     // console.log(response);
@@ -22,26 +25,18 @@ const SignIn = () => {
   //     userRedirect();
   //   }, []);
 
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   //   const logGoogleRedirectUser = async () => {
   //     const { user } = await signInWithGoogleRedirect();
   //     console.log(user);
   //   };
   return (
-    <div>
-      <h1>Sign In page</h1>
-      <button onClick={logGoogleUser}>Sign In with Google</button>
-      {/* <button onClick={signInWithGoogleRedirect}>
-        Sign In with Google Redirect
-      </button> */}
+    <div className="authentication-container">
+      {/* <h1>Sign In page</h1> */}
+      <SignInForm />
 
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
--- ecommerce/src/utils/firebase/firebase.utils.js
+++ ecommerce/src/utils/firebase/firebase.utils.js
@@ -7,6 +7,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -82,4 +83,10 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
 };
